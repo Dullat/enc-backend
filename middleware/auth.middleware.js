b8f9b9ef641333461a0a5e67878ea5ca200a81ad
dullat <142017979+Dullat@users.noreply.github.com>
2026-02-28 16:11:40 +0000

    created authMiddleware for protected routes

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const { Unauthorized } = require("../errors/Errors.error.js");
+
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+
+    if (!accessToken)
+      throw new Unauthorized("Access denied. No access token provided");
+
+    try {
+      const decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      throw new Unauthorized("Invalid or expired token");
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = authMiddleware;
