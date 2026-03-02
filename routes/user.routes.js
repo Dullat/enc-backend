a7bb2f0d111a7cfb264a58bb2d50c35c7264a9bf
dullat <142017979+Dullat@users.noreply.github.com>
2026-03-02 03:41:40 +0000

    setting, fixing and naming(better names) routs

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,15 +6,32 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  verifyEmail,
+  resendVerificationEmail,
+  refreshAccessToken,
+  requestPasswordReset,
 } = require("../controllers/auth.controller.js");
 
-const { verifyEmail } = require("../controllers/user.controller.js");
+const {
+  resetPassword,
+  renderResetPasswordForm,
+} = require("../controllers/user.controller.js");
 
 // ######### setting Routes ###########
 
+// core auth
 router.route("/api/auth/register").post(registerUser);
 router.route("/api/auth/login").post(loginUser);
-router.route("/api/auth/verify-email").post(verifyEmail);
 router.route("/api/auth/logout").post(logoutUser);
+router.route("/api/auth/refresh").post(refreshAccessToken);
+
+// email verification
+router.route("/api/auth/verify-email").get(verifyEmail);
+router.route("/api/auth/resend-verification").post(resendVerificationEmail);
+
+// password reset
+router.route("/api/auth/forget-password").post(requestPasswordReset);
+router.route("/api/auth/reset-password").get(renderResetPasswordForm);
+router.route("/api/auth/reset-password/:token").post(resetPassword);
 
 module.exports = router;
