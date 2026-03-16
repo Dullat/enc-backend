b8737e5cc3dec494e4bbb37f880f9cdbf3f95da9
dullat <142017979+Dullat@users.noreply.github.com>
2026-03-16 17:59:55 +0000

    prepare for deployment

--- middleware/errorhandler.middleware.js
+++ middleware/errorhandler.middleware.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Something went wrong...";
@@ -7,7 +9,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode).json({
     success: false,
     message: message,
-    stack: err.stack,
+    stack: process.env.NODE_ENV === "development" ? err.stack : "",
   });
 };
 
